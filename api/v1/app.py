6bb6d94ca52c305d41ed4a91b7cb8cbebe448b0c
Favourokerri <favourokerri767@gmail.com>
2023-10-02 03:11:03 +0000

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""app.py"""
+"""app.py main module"""
 import os
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 @app.teardown_appcontext
 def teardown_appcontext(code):
-    """teardown"""
+    """teardown_appcontext"""
     storage.close()
 
 
